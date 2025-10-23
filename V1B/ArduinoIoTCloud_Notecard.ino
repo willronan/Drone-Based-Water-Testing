4157884ee302425f9dbf77af6f078b2f71a8a020
William Ronan <97193884+willronan@users.noreply.github.com>
2025-10-23 14:10:37 -0700

    Update ArduinoIoTCloud_Notecard.ino

--- V1B/ArduinoIoTCloud_Notecard.ino
+++ V1B/ArduinoIoTCloud_Notecard.ino
@@ -18,6 +18,10 @@
 #include "SPI.h"
 #include <TinyGPSPlus.h>
 #include <SoftwareSerial.h>
+#include "Arduino.h"
+#include "MS5837.h"
+#include <Wire.h>
+
 
 /*
   HARDWARE DEFINITIONS
@@ -26,10 +30,20 @@
 static int const LED_BUILTIN = 2;                         // LED definition
 #endif
 
+
+/*
+  Basic control and commands for the PCA9548A/TCA9548A I2C multiplexer
+*/
+
+#define MUX_ADDR 0x70 //7-bit unshifted default I2C Address
+
+
 #define address 100       // I2C ID number for salinity sensor      
 char computerdata[32];    // buffer used for communication from computer to integrated sality sensor circuit
 
 TSYS01 tempSensor;        // Blues Robotics temperature sensor 
+
+MS5837 pressureSensor;
 
 TinyGPSPlus gps;
 
@@ -41,6 +55,16 @@ static const int cs = 21;
 // UART pins for GPS module
 static const int RXPin = 16, TXPin = 17;
 HardwareSerial gpsSerial(1);
+
+// Reference sea-level pressure (Pa)
+const float P0 = 101325.0;
+
+// Physical constants
+const float R = 8.3144598;     // J/(mol*K)
+const float g = 9.80665;       // m/s^2
+const float M = 0.0289644;     // kg/mol
+
+#define NUMBER_OF_SENSORS 3
 
 
 #define productUID "com.gmail.willronan4:drone"
@@ -58,6 +82,8 @@ byte in_char = 0;                // 1 byte buffer to store inbound bytes from th
 byte i = 0;                      // counter used for ec_data array
 char ec_data[32];                // 32 byte character array to hold incoming data from the EC circuit.
 
+float pressure = 0;
+
 /*
   LOCAL DATA SAVING VARIABLES
 */
@@ -70,6 +96,21 @@ int fileCount = 0;
  */
 // #define ATTN_PIN 9
 
+
+//Enables a specific port number
+void enableMuxPort(byte portNumber) {
+  if (portNumber > 7) return;
+  Wire.beginTransmission(MUX_ADDR);
+  Wire.write(1 << portNumber);
+  Wire.endTransmission();
+}
+
+void disableMuxPort(byte portNumber) {
+  (void)portNumber;  // Unused, but kept for clarity
+  Wire.beginTransmission(MUX_ADDR);
+  Wire.write(0);  // disable all ports
+  Wire.endTransmission();
+}
 
 /*
 
@@ -96,18 +137,56 @@ void setup() {
   Serial.println(1);
 
   // initialize sensor data 
-  double salinity = 0;
-  double temperature = 0;
-  double lat = 0.000000;
-  double lon = 0.000000;
-  double lastLat = 0.000000;
-  double lastLon = 0.000000;
+  lat = 0.000000;
+  lon = 0.000000;
 
   gpsSerial.begin(GPS_BAUD, SERIAL_8N1, RXPin, TXPin); 
 
+
   //  I2C
-  Wire.begin();                       //  I2C 
-  tempSensor.init();                  //  I2C
+  Wire.begin();
+  //Disable all eight mux ports initially, then we can enable them one at a time
+  for (byte x = 0 ; x <= 7 ; x++)
+  {
+    disableMuxPort(x);
+  }
+
+
+ // --- Port 0: MS5837 (pressure sensor) ---
+  enableMuxPort(0);
+  delay(50);
+  if (pressureSensor.init()) {
+    pressureSensor.setModel(MS5837::MS5837_02BA);
+
+    pressureSensor.setFluidDensity(997); // freshwater
+    Serial.println("Port 0: MS5837 initialized!");
+  } else {
+    Serial.println("Port 0: MS5837 init FAILED!");
+  }
+  disableMuxPort(0);
+
+  // --- Port 3: TSYS01 (temperature sensor) ---
+  enableMuxPort(3);
+  delay(50);
+  if (tempSensor.init()) {
+    Serial.println("Port 3: TSYS01 initialized!");
+  } else {
+    Serial.println("Port 3: TSYS01 init FAILED!");
+  }
+  disableMuxPort(3);
+
+  // --- Port 4: Atlas Salinity sensor ---
+  enableMuxPort(4);
+  delay(50);
+  Wire.beginTransmission(address);
+  byte error = Wire.endTransmission();
+  if (error == 0)
+    Serial.println("Port 4: Atlas Salinity sensor detected!");
+  else
+    Serial.println("Port 4: Atlas sensor NOT detected!");
+  disableMuxPort(4);
+
+  Serial.println("Initialization complete.\n");
 
   // connects variables to arduino cloud
   initProperties();
@@ -137,30 +216,50 @@ void loop() {
   // DATA COLLECTION HAPPENS HERE 
   if (led == true){
 
-    // collect data
-    temperature = tempSensor.temperature(); 
-    Serial.println(temperature);
-    salinity = readAtlasSensor();
+    //poll pressure sensor
+    enableMuxPort(0);
+    delay(10);
+    pressureSensor.read();
+    pressure = pressureSensor.pressure();
+    altitude = calculateAltitude(pressureSensor.pressure()*100, pressureSensor.temperature());
+    disableMuxPort(0);
+
+    //poll temp sensor
+    enableMuxPort(3);
+    delay(10);
     tempSensor.read();
+    temperature = tempSensor.temperature();
+    disableMuxPort(3);
+  
+    // --- Port 4: Atlas Salinity Sensor ---
+    enableMuxPort(4);
+    delay(10);
+    float salinity = readAtlasSensor();
+    disableMuxPort(4);
+
+
     getGPS(&lat, &lon); 
     location = Location(lat, lon);
-    time_and_date = getCardTime();
+    
+    delay(100);
 
-    //ArduinoIoTPreferredConnection.initiateNotehubSync();
-
-    Serial.print("Time: ");
-    Serial.println(time_and_date);
-
-    //getCellMetrics(&sinr, &rssi, &rsrp, &rsrq);
-
+    Serial.print("Salinity: ");
+    Serial.println(salinity);
+    Serial.print("Temperature: ");
+    Serial.println(temperature);
+    Serial.print("Pressure: ");
+    Serial.println(pressure);
+    Serial.print("Altitude");
+    Serial.println(altitude);
+    
+  
 
     // save in microSD file
     if(microSdSafety == true){
       Serial.println("calling writeToSD");
-      writeToSD(time_and_date, lat, lon, salinity, temperature, note, file);
+      writeToSD(time_and_date, lat, lon, altitude, salinity, temperature, note, file);
 
     }
-
 
   }
 }
@@ -220,6 +319,68 @@ void onNoteChange(){
 
 }
 
+
+float calculateAltitude(float P, float T_c) {
+  // Convert temp to Kelvin
+  float T = T_c + 273.15;
+
+  // Hypsometric equation
+  float factor = (R * T) / (g * M);
+  return factor * log(P0 / P);
+}
+
+void getGPS(double* lat, double* lon){   
+  Serial.println("getting GPS");
+  if (!gpsSerial.available()){
+    error = "GPS disconnected";
+  }
+
+  while (gpsSerial.available() > 0) {
+    gps.encode(gpsSerial.read());
+  }
+  if (gps.location.isUpdated()) {
+    *lat = gps.location.lat();
+    *lon = gps.location.lng();
+  } else {
+    Serial.print(F("INVALID")); 
+
+    *lat = -27.108573;
+    *lon = -109.291542;
+  }
+}
+
+// COLLECT TIME STAMP FOR DATA POINTS
+String getCardTime(){
+    // get current time
+  J *req = NoteNewRequest("card.time");
+  if (J *rsp = NoteRequestResponse(req))
+  {
+
+    int time = JGetNumber(rsp, "time");
+    int minutes = JGetNumber(rsp, "minutes");
+    String time_and_date = getDateTimeFromUnix(time, minutes);
+
+    NoteDeleteResponse(rsp);
+    return time_and_date;
+  }
+}
+
+
+// change a unix timestamp to a date time variable
+String getDateTimeFromUnix(time_t time, int minutesOffset) {
+
+  time_t adjustedTime = time + (minutesOffset * 60);  // Adjust for timezone
+  struct tm timeInfo;
+  gmtime_r(&adjustedTime, &timeInfo);  // Convert to UTC struct tm
+
+  char buffer[25];  // Enough space for "YYYY-MM-DDTHH:MM:SSZ"
+  snprintf(buffer, sizeof(buffer), "%04d-%02d-%02dT%02d:%02d:%02dZ",
+            timeInfo.tm_year + 1900, timeInfo.tm_mon + 1, timeInfo.tm_mday,
+            timeInfo.tm_hour, timeInfo.tm_min, timeInfo.tm_sec);
+
+  return String(buffer);
+}
+
 // COLLECT SALINITY SENSOR DATA
 float readAtlasSensor(){
   Wire.beginTransmission(address);                                            //call the circuit by its ID number.                        // temp calibration
@@ -265,60 +426,7 @@ float readAtlasSensor(){
 
 }
 
-void getGPS(double* lat, double* lon){   
-  Serial.println("getting GPS");
-  if (!gpsSerial.available()){
-    error = "GPS disconnected";
-  }
-
-  while (gpsSerial.available() > 0) {
-    gps.encode(gpsSerial.read());
-  }
-  if (gps.location.isUpdated()) {
-    *lat = gps.location.lat();
-    *lon = gps.location.lng();
-  } else {
-    Serial.print(F("INVALID")); 
-
-    *lat = -27.108573;
-    *lon = -109.291542;
-  }
-}
-
-
-// COLLECT TIME STAMP FOR DATA POINTS
-String getCardTime(){
-    // get current time
-  J *req = NoteNewRequest("card.time");
-  if (J *rsp = NoteRequestResponse(req))
-  {
-
-    int time = JGetNumber(rsp, "time");
-    int minutes = JGetNumber(rsp, "minutes");
-    String time_and_date = getDateTimeFromUnix(time, minutes);
-
-    NoteDeleteResponse(rsp);
-    return time_and_date;
-  }
-}
-
-
-// change a unix timestamp to a date time variable
-String getDateTimeFromUnix(time_t time, int minutesOffset) {
-
-  time_t adjustedTime = time + (minutesOffset * 60);  // Adjust for timezone
-  struct tm timeInfo;
-  gmtime_r(&adjustedTime, &timeInfo);  // Convert to UTC struct tm
-
-  char buffer[25];  // Enough space for "YYYY-MM-DDTHH:MM:SSZ"
-  snprintf(buffer, sizeof(buffer), "%04d-%02d-%02dT%02d:%02d:%02dZ",
-            timeInfo.tm_year + 1900, timeInfo.tm_mon + 1, timeInfo.tm_mday,
-            timeInfo.tm_hour, timeInfo.tm_min, timeInfo.tm_sec);
-
-  return String(buffer);
-}
-
-void writeToSD(String wtime, double latitude, double longitude, double sal, double temp, String notepad, File &f){
+void writeToSD(String wtime, double latitude, double longitude, double alt, double sal, double temp, String notepad, File &f){
   Serial.println("In writeToSD");
   f.print(notepad);
   f.print(F(","));
@@ -327,6 +435,8 @@ void writeToSD(String wtime, double latitude, double longitude, double sal, doub
   f.print(latitude, 6);
   f.print(F(","));
   f.print(longitude, 6);
+  f.print(F(","));
+  f.print(alt);
   f.print(F(","));
   f.print(sal);
   f.print(F(","));
@@ -387,3 +497,5 @@ void signalErrorWithLED(){
     delay(1000);
   }
 }
+
+
